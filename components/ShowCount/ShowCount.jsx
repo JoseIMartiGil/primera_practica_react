859be0c1921312f0dec8309bdd5ccc24bf2f7468
JoseIMartiGil <165684119+JoseIMartiGil@users.noreply.github.com>
2024-09-21 18:19:55 +0200

    Add files via upload

---
+++ components/ShowCount/ShowCount.jsx
@@ -0,0 +1,11 @@
+import './ShowCount.css';
+
+const ShowCount = (props) => {
+  return (
+    <div className='showcount'>
+         <h2>You clicked here {props.count} {props.count === 1 ? "time" : "times"}</h2>
+    </div>
+  );
+};
+
+export default ShowCount;
